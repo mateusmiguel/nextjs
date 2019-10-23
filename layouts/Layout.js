c8e4f91dfc58dae7e9fa11d8ac36dd71543023d5
Mateus Miguel <webmateusmiguel@gmail.com>
2019-10-23 17:39:45 -0300

    Search Icon

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -7,8 +7,10 @@ const Layout = ({children}) => {
   return (
     <>
       <header className="header">
+        <div className="contained">
           <div className="logo">Next<span>.js</span></div>
           <Menu />
+        </div>
       </header>
       
       <main className="main">
--- pages/search.js
+++ pages/search.js
@@ -25,22 +25,23 @@ class Search extends React.Component {
 
   componentDidMount() {
     fetch("http://nodeserver.local.debug.site:8080/convenios")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            partners: result.data.data,
-            findedPartners: result.data.data,
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      );
+    .then(res => res.json())
+    .then(
+      (result) => {
+        result.data.data.sort((a, b) => (a.nomeFantasia > b.nomeFantasia) ? 1 : -1)
+        this.setState({
+          isLoaded: true,
+          partners: result.data.data,
+          findedPartners: result.data.data,
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
   }
 
   handleSearch = debounce(searchTerm => {
@@ -52,34 +53,29 @@ class Search extends React.Component {
   render() {
     const { error, isLoaded, findedPartners } = this.state;
 
-    // if (error) {
-    //   return <div>Error: {error.message}</div>;
-    // } else if (!isLoaded) {
-    //   return <div>Loading...</div>;
-    // } else {
-
     return (
       <Layout>
-        <h1>Search</h1>
+        <h3>Busca</h3>
 
         {error ? (
           <p>Não foi possível carregar a lista de parceiros no momento. Tente novamente em alguns instantes.</p>
         ) : !isLoaded? (
-          <p>carregando...</p>
+          <p>Carregando...</p>
         ) : (
             <div className="search">
 
               <form onSubmit={e => { e.preventDefault(); }}>
                 <input
-                  placeholder="Search for..."
+                  placeholder="Busque uma empresa..."
                   onChange={e => { this.handleSearch(e.target.value) }}
                 />
+                <button type="submit">S</button>
               </form>
               <ul>
                 {findedPartners &&
                   findedPartners.map((item, key) => (
                     <li key={key}>
-                      {item.nomeFantasia}
+                      {item.nomeFantasia.toLowerCase()}
                     </li>
                   ))
                 }
@@ -90,6 +86,5 @@ class Search extends React.Component {
     );
   }
 }
-// }
 
 export default Search;
